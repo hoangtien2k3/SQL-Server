8587664c9831714d0a4431712982fb163ca4253e
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-03-01 00:05:10 +0700

    SQL Server

---
+++ 3_Tạo,Xóa,SửaBảng/SQLQuery10.sql
@@ -0,0 +1,32 @@
+﻿CREATE DATABASE SQLServer
+GO
+
+-- SU DUNG SQLServer
+USE SQLServer
+GO
+
+CREATE TABLE HocSinh
+(
+	MaSV NVARCHAR(10),
+	Name NVARCHAR(50)
+)
+GO
+
+CREATE TABLE GiaoVien
+(
+	MaGV NVARCHAR(10),
+	Name NVARCHAR(50)
+)
+GO
+
+-- alter dùng để sử thông tin, còn add là dùng để thêm thông tin vào table database hocsinh
+ALTER TABLE HocSinh ADD NgaySinh Date
+
+
+-- xóa toàn bộ dữ liệu của bảng.
+TRUNCATE TABLE HocSinh
+
+
+-- xoa toan bo table hocsinh
+DROP TABLE HocSinh
+GO
---
+++ 4_KiểuDữLiệu/KieuDuLieu.sql
@@ -0,0 +1,23 @@
+﻿ -- các kiểu ký tự hay dùng
+
+ -- int : kiểu số nguyên
+ -- float : kiểu số thực
+ -- char : kiểu ký tưj, bộ nhớ cấp phát cứng
+ -- varchar : kiểu ký tự, bộ nhớ cấp phát động
+ -- nchar : kiểu  ký tự. có thể lưu tiếng việt
+ -- nvarchar: kiểu ký tự cấp phá động có thể lưu tiếng việt.
+ -- date : lưu ngày tháng năm ...
+ -- time : lưu giờ, phút, giây ...
+ -- byte : lưu giá trị 0 - 1
+ -- text : lưu văn bản lớn
+ -- ntext : lưu văn bản lớn có tiếng việt.
+
+
+ CREATE TABLE TEST
+ (
+	VanBan NVARCHAR(50), -- KHAI BAO NVARCHAR CAP PHAT DONG 50 O NHO.
+	MaSV CHAR(10), -- KHAO BAO TRUONG MA SV KIEU CHAR CAP PHAT CUNG 10 O NHO.
+	Birthday DATE,
+	Sex BIT, -- luu gia tri 0 or 1
+ )
+ GO
