eb6159ba8ec124b28a55b7be836be760b495bcad
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-03-02 01:30:09 +0700

    SQL Server

--- 7_TạoKhóaNgoại/SQLQuery3.sql
+++ 7_TạoKhóaNgoại/SQLQuery3.sql
@@ -20,24 +20,31 @@ CREATE TABLE Lop
 )
 Go
 
-CREATE TABLE GiaoVien
+
+-- điều kiện cần đê tạo khóa ngoại, 
+-- tham chiếu tới khóa chính
+-- cùng kiểu dữ liệu
+-- unique not null
+
+-- lợi ích: đảm bảo toàn bộ dữ liệu và không có tham chiếu tới dữ liệu không tồn tại
+-- tạo khóa ngoại trong table
+CREATE TABLE Giaovien
 (
 	MaGV char(10) not null,
 	Name nvarchar(100) default N'Tên giáo viên',
 	DiaChi nvarchar(100) default N'Địa chỉ giáo viên',
-
 	NgaySinh date,
+	Sex bit, 
 
-	Sex bit
+	-- cách tạo khóa ngoại ngay khi tạo bảng
+	MaBM char(10) FOREIGN KEY(MaBM) REFERENCES dbo.BoMon(MaBM)
 )
 Go
 
 
-CREATE TABLE Hocsinh
-(
-	Mahs char(10) primary key,
-	Name nvarchar(100)
-)
-Go
+-- tạo khóa ngoại ngoại sau khi  tạo table
+alter table dbo.Hocsinh add constraint FK_Hocsinh foreign key(Malop) references dbo.Lop(Malop)
 
+
+alter table dbo.Hocsinh Drop constraint FK_Hocsinh
 
---
+++ 9_TruyVấnVớiĐiềuKiện/SQLQuery1.sql
@@ -0,0 +1,50 @@
+﻿
+Use HoangTien2k3
+Go
+
+Select GV.MAGV, GV.HOTEN, NT.TEN from dbo.GIAOVIEN AS GV, dbo.NGUOITHAN AS NT
+Where GV.MAGV = NT.MAGV
+
+-- lấy ra lương > 2000
+SELECT *FROM dbo.GIAOVIEN
+Where LUONG > 2000
+
+
+-- lấy ra giáo viên là nữ và lương > 2000
+Select * from dbo.GIAOVIEN
+Where LUONG > 2000 and PHAI = N'Nữ'
+
+
+-- lấy ra giáo viên nhỏ hơn 30 tuổi.
+Select * from dbo.GIAOVIEN
+Where YEAR(GETDATE()) - YEAR(NGSINH) > 40 
+
+
+-- lấy ra giáo viên, năm sinh và tuổi của giáo viên nhỏ hơn 40 tuổi
+Select HOTEN, NGSINH, YEAR(GETDATE()) - YEAR(NGSINH) from dbo.GIAOVIEN 
+Where YEAR(GETDATE()) - YEAR(NGSINH) >= 40 
+
+
+-- lấy ra giáo viên là trưởng hộ môn
+Select gv. * From dbo.GIAOVIEN as gv, dbo.BOMON as bm
+Where gv.MAGV = bm.TRUONGBM 
+
+
+
+-- đếm số lượng giáo viên
+-- COUNT(*) -> Đếm số lượng của tên trường đó.
+-- COUNT(tên trường nào đó) -> đếm số lượng của tên trường đó
+Select COUNT(*) AS N'Số lượng giáo viên' from dbo.GIAOVIEN
+
+
+
+-- đếm số lượng người thân của giáo viên có mã GV là 007
+Select * 
+From dbo.GIAOVIEN, dbo.NGUOITHAN
+Where GIAOVIEN.MAGV = '007' AND GIAOVIEN.MAGV = NGUOITHAN.MAGV
+
+
+-- lấy ra tên giáo viên và tên đề tài người đó tham gia
+Select HOTEN, TENDT 
+From dbo.GIAOVIEN, dbo.THAMGIADT, dbo.DETAI
+Where GIAOVIEN.MAGV = THAMGIADT.MAGV AND DETAI.MADT = THAMGIADT.MADT
