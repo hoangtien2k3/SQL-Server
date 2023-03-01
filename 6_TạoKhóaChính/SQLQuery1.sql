638685a0ec5d6375ae821d4d60fc9a994dbd506b
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-03-01 16:32:41 +0700

    SQL Server

---
+++ 6_TạoKhóaChính/SQLQuery1.sql
@@ -0,0 +1,66 @@
+﻿
+-- unique : một ràng buộc (constraint) trên cột. Trường nào có từ khóa UNIQUE thì không	 thể có 2 giá trị trùng nhau.
+-- "NOT NULL": yêu cầu cột không được phép có giá trị null (rỗng).
+-- default: giá trị mặc định của trường đó nếu không gán giá trị insert
+
+CREATE TABLE TestPrimaryKey1
+(
+	ID int UNIQUE NOT NULL default 1,
+	Name nvarchar(100) default N'HOANGTIEN2K3'
+
+)
+GO
+
+insert dbo.TestPrimaryKey1
+	(
+		ID
+		--Name
+	)
+values
+	(
+		1234
+	)
+GO
+
+insert dbo.TestPrimaryKey1
+	(
+		ID
+		--Name
+	)
+values
+	(
+		456
+	)
+GO
+insert dbo.TestPrimaryKey1
+	(
+		ID
+		--Name
+	)
+values
+	(
+		3123
+	)
+GO
+insert dbo.TestPrimaryKey1
+	(
+		ID
+		--Name
+	)
+values
+	(
+		5235123
+	)
+GO
+
+
+-- tạo 2 khóa chính.
+CREATE TABLE TestPrimaryKey3
+(
+	ID1 int not null,
+	ID2 int not null,
+	Name nvarchar(100) default N'hoangtien2k3'
+
+	primary key (ID1, ID2)
+)
+GO
---
+++ 7_TạoKhóaNgoại/SQLQuery3.sql
@@ -0,0 +1,43 @@
+﻿CREATE DATABASE Primary_Foreign
+Go
+
+use Primary_Foreign
+GO
+
+CREATE TABLE BoMon
+(
+	MaBM char(10) primary key,
+	Name nvarchar(100) default N'Tên Bộ Môn'
+)
+GO
+
+CREATE TABLE Lop
+(
+	Malop char(10) not null,
+	Name nvarchar(100) default N'Tên lớp',
+
+	primary key(Malop)
+)
+Go
+
+CREATE TABLE GiaoVien
+(
+	MaGV char(10) not null,
+	Name nvarchar(100) default N'Tên giáo viên',
+	DiaChi nvarchar(100) default N'Địa chỉ giáo viên',
+
+	NgaySinh date,
+
+	Sex bit
+)
+Go
+
+
+CREATE TABLE Hocsinh
+(
+	Mahs char(10) primary key,
+	Name nvarchar(100)
+)
+Go
+
+
